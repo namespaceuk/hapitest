12fbfe82aa98df6e976ad99dc629271cc989e546
Richard Lyon <namespaceuk@yahoo.com>
2016-07-10 09:39:12 +0100

    staged commit

---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var Database = require('./database');
+var Hapi = require('hapi');
+
+var database = new Database();
+var server = new Hapi.Server({debug: {request: ['info', 'error']}});
+
+// Expose database
+if (process.env.NODE_ENV === 'test') {
+    server.database = database;
+}
+
+// Create server
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
+
+// Add routes
+var plugins = [
+    {
+        register: require('./routes/tasks.js'),
+        options: {
+            database: database
+        }
+    }
+];
+
+//route info log message to the console
+server.on('log', (event, tags) => {
+
+    if (tags.info) {
+        console.log(event);
+    }
+});
+
+server.register(plugins, function (err) {
+    if (err) { throw err; }
+
+    if (!module.parent) {
+        server.start(function(err) {
+            if (err) { throw err; }
+
+            server.log('info', 'Server running at: ' + server.info.uri);
+        });
+    }
+});
+
+module.exports = server;
